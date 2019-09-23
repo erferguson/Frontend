1bd749598ac07924c22a4a214b362e8f888846b7
Hector Valdez <hvaldez180@gmail.com>
2019-09-23 16:59:42 -0400

    added some titles and styles

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,9 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+
+const Header = styled.h3`
+padding-top: 5%;
+padding-bottom: 3%;
+text-align: center;
+background-color: black;
+color: white;
+`;
+
+const Title = styled.h2`
+text-align: center;
+`;
 
 export default function LoginScreen () {
     return (
         <div>
-            
+            <Header>Log in</Header>
+            <Title>Welcome back</Title>
         </div>
     )
 };
--- src/views/App.js
+++ src/views/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Route } from 'react-router-dom';
+import LoginScreen from '../components/LoginScreen';
 
 function App() {
     return (
-        <h1>It works!</h1>
+    <div>
+        <Route path="/login-page" component={LoginScreen}/>
+    </div>
     )
 }
 
